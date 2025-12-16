e65dc3c52f2c310e834218e6c0f1369adb4a23b1
tse kam pui <1285364215@qq.com>
2025-12-16 15:59:20 +0800

    更改训练模型

--- model.py
+++ model.py
@@ -13,21 +13,26 @@ class InverseMLP(nn.Module):
         # 定义网络结构
         self.model = nn.Sequential(
             # 输入层: 300 (3*100) 个特征
-            nn.Linear(input_size, 256),
+            nn.Linear(input_size, 2048),
             nn.ReLU(),
             nn.Dropout(0.2),  # 添加 Dropout 防止过拟合
 
-            # 隐藏层 1
-            nn.Linear(256, 512),
+            # 隐藏层
+            nn.Linear(2048, 256),
+            nn.ReLU(),
+            nn.Dropout(0.2),
+
+            # 隐藏层
+            nn.Linear(256, 32),
             nn.ReLU(),
             nn.Dropout(0.2),
 
             # 隐藏层 2
-            nn.Linear(512, 256),
+            nn.Linear(32, 64),
             nn.ReLU(),
 
             # 输出层: 7 个物理参数
-            nn.Linear(256, output_size),
+            nn.Linear(64, output_size),
 
             # 我们预测的是 [0, 1] 范围内的归一化值，Sigmoid 是一个好选择
             # 但在实践中，直接线性输出（下面这行）通常更稳健
